c91eaaff031662ef175d9c3fb30dbd96cbd6edca
Sahus Gupta <63523632+sahusgupta@users.noreply.github.com>
2024-11-21 10:05:57 -0600

    Update model.py

--- components/Transformer/model.py
+++ components/Transformer/model.py
@@ -1,13 +1,23 @@
 from transformer import Transformer
+from torch import nn
+import torch
 
-class TransformerLayer():
-    def __init__(self, input: iter):
-        self.input = input
-        self.out = Transformer()
+class RouterTransformer(nn.Module):
+    def __init__(self, transformers, config):
+        super().__init__()
+        self.transformers = nn.ModuleList(transformers)
+        self.router = nn.Linear(config['input_config'], len(transformers))
 
-# figure out how to process data into comprehensible word vectors.
+    def forward(self, src, tgt):
+        rout_weights = torch.softmax(self.router(src), dim=1)
 
-    def forward(self):
-        self.out.forward(input)
+        trans_outs = [
+            transformer(src, tgt) for transformer in self.transformers
+        ]
 
-      
+        comb_out = sum(
+            output * weight.unsqueeze(-1).unsqueeze(-1) 
+            for output, weight in zip(trans_outs, rout_weights.T)
+        )
+
+        return comb_out
