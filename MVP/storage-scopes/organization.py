10e6f817eaea797227c2b6215b7ef14b500f7f66
Sahus Gupta <sahusgupta@gmail.com>
2025-01-30 11:41:13 -0600

    scopes for gcs access

--- MVP/storage-scopes/organization.py
+++ MVP/storage-scopes/organization.py
@@ -1,5 +1,5 @@
 from google.cloud import storage
-
+from google.api_core import page_iterator
 def create_district(name, uniqueID):
     client = storage.Client()
     bucket = client.bucket(name + "_" + uniqueID)
@@ -11,3 +11,24 @@ def remove_district(name):
     bucket = client.get_bucket(name)
     bucket.delete()
 
+def list_teachers(district, school):
+
+    extra_params = {
+        "projection": "noAcl",
+        "delimiter": '/'
+    }
+
+    gcs = storage.Client()
+    
+    path = f"/b/{district}/o"
+
+    iterator = page_iterator.HTTPIterator(
+        client=gcs,
+        api_request=gcs._connection.api_request,
+        path=path,
+        items_key='prefixes',
+        item_to_value=lambda _, item: item.rstrip('/').split('/')[-1],
+        extra_params=extra_params,
+    )
+
+    return [school for school in iterator if school]
--- MVP/storage-scopes/teacher.py
+++ MVP/storage-scopes/teacher.py
@@ -1,1 +1,4 @@
 from google.cloud import storage
+
+def add_teacher(school, teacher):
+    client = 
